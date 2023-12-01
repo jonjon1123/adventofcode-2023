dec80758f75f64ca697683af9c981c22a98fa36a
Jon Terry <jonjon1123@gmail.com>
2023-12-01 15:32:17 -0600

    Finished day 1 part 2

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -6,4 +6,9 @@ describe('On Day 1', () =>{
         const content = await fs.promises.readFile(`./inputs/day1/part1.txt`);
         expect(day1.solveForPartOne(content.toString())).toBe('53386');
     })
+
+    it(`part2 is identity function`, async ()=>{
+        const content = await fs.promises.readFile(`./inputs/day1/part2.txt`);
+        expect(day1.solveForPartTwo(content.toString())).toBe('53312');
+    })
 });
--- src/day1/index.ts
+++ src/day1/index.ts
@@ -22,7 +22,60 @@ class Day1 extends Day {
   }
 
   solveForPartTwo(input: string): string {
-    return input;
+    const spelledOutNumbers: Map<string, string> = new Map();
+    spelledOutNumbers.set('one', '1');
+    spelledOutNumbers.set('two', '2');
+    spelledOutNumbers.set('three', '3');
+    spelledOutNumbers.set('four', '4');
+    spelledOutNumbers.set('five', '5');
+    spelledOutNumbers.set('six', '6');
+    spelledOutNumbers.set('seven', '7');
+    spelledOutNumbers.set('eight', '8');
+    spelledOutNumbers.set('nine', '9');
+
+    const spelledOutNumbersRegex = Array.from(spelledOutNumbers.keys()).join("|");
+
+    let total: number = 0;
+    const lines = input.split("\n");
+    for (const lineIndex in lines) {
+      const regex = new RegExp("\\d|(?=("+spelledOutNumbersRegex+"))", 'g');
+      const line = lines[lineIndex];
+      let m;
+      const numbers: string[] = [];
+
+      while ((m = regex.exec(line)) !== null) {  
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+          regex.lastIndex++;
+        }
+        
+        // The result can be accessed through the `m`-variable.
+        m.forEach((match, groupIndex) => {
+          if (match) {
+            numbers.push(match);
+          } 
+        });
+      }
+           
+      
+      if (numbers != null) {
+        let firstNumber: string = numbers[0];
+        let lastNumber: string = numbers[numbers.length-1];
+
+        // If the number can't be parsed to an int, we know it is one of the spelled out numbers
+        if (isNaN(parseInt(firstNumber))) {
+          firstNumber = spelledOutNumbers.get(firstNumber)!;
+        }
+
+        if (isNaN(parseInt(lastNumber))) {
+          lastNumber = spelledOutNumbers.get(lastNumber)!;
+        }
+
+        total = total + parseInt(firstNumber+lastNumber);
+      }
+      
+    }
+    return total.toString();
   }
 }
 
